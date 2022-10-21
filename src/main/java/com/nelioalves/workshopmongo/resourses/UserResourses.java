5f421bdabb42ebf9c22ee3be64546f6f481b03bb
Ismael Moreira <imcalazans@gmail.com>
2022-10-21 11:29:44 -0300

    Entity User and REST Running

---
+++ src/main/java/com/nelioalves/workshopmongo/resourses/UserResourses.java
@@ -0,0 +1,31 @@
+package com.nelioalves.workshopmongo.resourses;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResourses {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){
+		User maria = new User("1", "Maria Brown", "maria@gmail.com");
+		User alex = new User("2", "Alex Green", "alex@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	
+	
+
+}

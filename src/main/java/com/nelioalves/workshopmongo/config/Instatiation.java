e534b3b132cae80a59b9b042c08031901a19f9b3
Ismael Moreira <imcalazans@gmail.com>
2022-11-05 07:19:07 -0300

    Consulta com vários critérios

--- src/main/java/com/nelioalves/workshopmongo/config/Instatiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instatiation.java
@@ -40,7 +40,7 @@ public class Instatiation implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria)); 
-		Post post2 = new Post(null, sdf.parse("2018/03/23"),  "Bom dia",  "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"),  "Bom dia",  "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
 		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -11,9 +12,11 @@ import com.nelioalves.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-	List<Post> findByTitleContainingIgnoreCase(String text);
-	
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} } , {date: { $lte: ?2 }}, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date mmaxDate);
 }
